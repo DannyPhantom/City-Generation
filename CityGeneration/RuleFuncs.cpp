61868074aa331baf26445101999234d3901a9f5f
marc.desorcy <marc.desorcy@zone44-ec.cs.ucalgary.ca>
2017-03-24 15:52:23 -0600

    A start on the new class for rule functions

---
+++ CityGeneration/RuleFuncs.cpp
@@ -0,0 +1,22 @@
+/*
+ * RuleFuncs.cpp
+ *
+ *  Created on: Mar 24, 2017
+ *      Author: marc.desorcy
+ */
+
+#include "RuleFuncs.h"
+
+RuleFuncs::RuleFuncs() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RuleFuncs::~RuleFuncs() {
+	// TODO Auto-generated destructor stub
+}
+
+Scope RuleFuncs::S(float x, float y, float z, std::string name)
+{
+
+}
---
+++ CityGeneration/RuleFuncs.h
@@ -0,0 +1,50 @@
+/*
+ * RuleFuncs.h
+ *
+ *  Created on: Mar 24, 2017
+ *      Author: marc.desorcy
+ */
+
+#ifndef CITYGENERATION_RULEFUNCS_H_
+#define CITYGENERATION_RULEFUNCS_H_
+
+/*NOTICE: Make new class!!! (call it whatever)
+ * Create functions for S(), T() Comp(), Subdiv(), Repeat(), and R()
+ * S() - Takes a scope, returns a copy of the scope with the given size.
+ *       Scope will be a class (Bernie's working on it). Has rotation, translation, scale
+ * T() - Sets translation vector on scope.
+ * R() - Sets rotation as vectors on scope (only doing one axis).
+ * 		- Make sure to have a parameter to specify WHICH axis
+ * Comp(type, param){A|B|...|Z} - Will be tough. Given a scope, create more scopes out of it.
+ * 		Each Scope is a 2Dplane. EG: If type=='sidefaces', then generate four different scopes,
+ * 		each is a 2D-plane with the parameters (x, y, 0). Some will have a rotation factor applied.
+ * 		A translation factor, too.
+ * 		Will also deal with faces, edges. (Ignore vertices for now)
+ * 	Subdiv() - take an axis, a vector of values, and a vector of names. Return a vector of
+ * 			scopes, one per name. This subdivides the scope into different parts with their own names.
+ * 			This should also be able to deal with double-axes (like XY, XZ), but ignore this for nwo.
+ * 	Repeat() - Takes the axis of a 2D-plane, a value (eg. window-spacing), and a number n.
+ * 				n = 2D-plane's length along the axis/value. This subdivides the scope into
+ * 				n scopes, all with the same name and size.
+ * 				Warning: there's a chance length/window_spacing is not an integer. In this case:
+ * 				take length/window_spacing, round the answer, take length/(rounded_answer), get new_window_spacing
+ * 				Use the new spacing.
+ *
+ * 	REMEMBER: Comp, Subdiv and Repeat need to take a list of new scope names as parameters, then return
+ * 	scopes with those names.
+ *
+ *
+ *
+ * */
+
+#include Scope.h
+
+class RuleFuncs {
+public:
+	RuleFuncs();
+	virtual ~RuleFuncs();
+
+	Scope S(float x, float y, float z, std::string name);
+};
+
+#endif /* CITYGENERATION_RULEFUNCS_H_ */
